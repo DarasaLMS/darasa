e1dcae5589170ebe3d75c368848158e328581eb7
Antony Orenge <antoroko@gmail.com>
2020-07-20 13:56:28 +0300

    Remove email from required fields

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -108,7 +108,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "email"]
+    REQUIRED_FIELDS = ["first_name",]
     objects = UserManager()
 
     __email = None
