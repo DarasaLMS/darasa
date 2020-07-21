54d3763533d5c8259692b37e1f74ae643512d104
Antony Orenge <antoroko@gmail.com>
2020-07-21 20:29:13 +0300

    Add admin views

--- apps/classrooms/admin.py
+++ apps/classrooms/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Course, Topic, Classroom, Request
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+    list_display = ("title", "description", "date_modified")
+    date_hierarchy = "date_modified"
+    ordering = ["-date_modified"]
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    model = Topic
+    list_display = ("title", "description", "course", "date_modified")
+    date_hierarchy = "date_modified"
+    ordering = ["-date_modified"]
+
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    model = Classroom
+    list_display = ("name", "course", "topic", "meeting_id", "teacher", "date_modified")
+    date_hierarchy = "date_modified"
+    ordering = ["-date_modified"]
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    model = Request
+    list_display = ("classroom", "student", "teacher", "status", "date_modified")
+    date_hierarchy = "date_modified"
+    ordering = ["-date_modified"]
