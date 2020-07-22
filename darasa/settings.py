61cb42e3a42caf9073949443ff1fa8bdc9143fe5
Antony Orenge <antoroko@gmail.com>
2020-07-22 21:51:31 +0300

    Add fontawesome 5 package

--- darasa/settings.py
+++ darasa/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "fontawesome_5",
     "rest_framework",
     "django_filters",
     "drf_yasg",
