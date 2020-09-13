2153c3c7365eac622c39e3ea94b4a920cecde92e
Antony Orenge <antoroko@gmail.com>
2020-09-13 13:40:26 +0300

    Remove api names

--- apps/classrooms/api/urls.py
+++ apps/classrooms/api/urls.py
@@ -20,27 +20,13 @@ urlpatterns = [
     re_path(r"^courses/(?P<course_id>.+)/requested/$", has_requested_course),
     re_path(r"^courses/(?P<course_id>.+)/joined/$", has_joined_course),
     re_path(r"^courses/(?P<course_id>.+)/$", CourseView.as_view()),
-    re_path(r"^classrooms/$", ClassroomCreateView.as_view(), name="api_classrooms"),
-    re_path(
-        r"^classrooms/(?P<classroom_id>.+)/$",
-        ClassroomView.as_view(),
-        name="api_classrooms",
-    ),
+    re_path(r"^classrooms/$", ClassroomCreateView.as_view()),
+    re_path(r"^classrooms/(?P<classroom_id>.+)/$", ClassroomView.as_view()),
     re_path(r"^rooms/(?P<room_id>.+)/join/$", create_join_meeting_room_link),
     re_path(r"^rooms/(?P<room_id>.+)/end/$", end_meeting),
     re_path(r"^rooms/(?P<room_id>.+)/running/$", check_running_meeting),
     re_path(r"^requests/$", create_request_view),
-    re_path(
-        r"^requests/(?P<request_id>.+)/$", RequestView.as_view(), name="api_requests"
-    ),
-    re_path(
-        r"^users/(?P<user_id>.+)/classrooms/$",
-        UserClassroomsView.as_view(),
-        name="api_user_classrooms",
-    ),
-    re_path(
-        r"^users/(?P<user_id>.+)/courses/$",
-        UserCoursesView.as_view(),
-        name="api_user_courses",
-    ),
+    re_path(r"^requests/(?P<request_id>.+)/$", RequestView.as_view()),
+    re_path(r"^users/(?P<user_id>.+)/classrooms/$", UserClassroomsView.as_view()),
+    re_path(r"^users/(?P<user_id>.+)/courses/$", UserCoursesView.as_view()),
 ]
