1ef48293db932ccd312e8f5ffb04e0bc4e5b1f7e
Antony Orenge <antoroko@gmail.com>
2021-01-24 17:31:15 +0200

    Remove nested UserSerializer from MessageSerializer

--- apps/comms/api/serializers.py
+++ apps/comms/api/serializers.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
-from apps.accounts.api.serializers import UserSerializer
 from ..models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    from_user = UserSerializer(many=False, read_only=True)
-    to_user = UserSerializer(many=False, read_only=True)
-
     class Meta:
         model = Message
         fields = [
