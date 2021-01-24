e23baa8ccef929ffd79fe4bd014b906178522cd9
Antony Orenge <antoroko@gmail.com>
2021-01-24 16:07:18 +0200

    Add query filter by category and order results by recently modified

--- apps/comms/api/views.py
+++ apps/comms/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Message
 from .serializers import MessageSerializer
 
@@ -8,3 +9,6 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["category"]
+    ordering = ["-date_modified"]
